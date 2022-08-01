2f56c06e6c91dd1f1dadfd0ebe14c2e1754134be
Bondar <bondar.aa@regenergy.ru>
2022-08-01 22:18:47 +0300

    color random

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  const onClick = () => {
-    console.log('hello');
-  }
-
-  return (
-    <View style={styles.container}>
-        <Text>Я уже почти четыре</Text>
-      <Button onPress={onClick} title={'Нажми, чтобы показать оповещение'} />
-      <StatusBar style="auto" translucent={false}/>
-    </View>
-  );
+    const [bgColor, setBgColor] = useState('fff');
+    const onClick = () => {
+        const color = Math.floor(Math.random()*16777215).toString(16);
+        setBgColor(color)
+    }
+    console.log('#' + bgColor)
+    return (
+        <View style={styles({backgroundColor: bgColor}).container}>
+            <Text>Любая надпись</Text>
+            <Button onPress={onClick} title={'Нажми, чтобы показать оповещение'}/>
+            <StatusBar style="auto" translucent={false}/>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-  },
+const styles = (props) => StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#' + props.backgroundColor,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 });
